c5cd330a080cf4c102cd3448e4c9682c527716ae
Darshan Ramjiyani <ahamdsp@protonmail.com>
2023-05-04 11:35:55 +0530

    Update IdelePrevention.py

--- IdelePrevention.py
+++ IdelePrevention.py
@@ -1,11 +1,35 @@
 # author Darshan Ramjiyani
 
-import pyautogui as pag
+import threading
+import time
+import pyautogui
 
-if __name__=="__main__":
-    from time import sleep
+
+def click_thread():
     while True:
-        x,y = pag.position()
-        print(f"Clicking at the : ({x}, {y}).")
-        pag.click()
-        sleep(1)
+        pyautogui.click()  # perform a mouse click action
+        time.sleep(3)  # wait for half a second
+
+
+def scroll_thread():
+    while True:
+        pyautogui.scroll(-1)  # scroll down by one unit
+        time.sleep(1)  # wait for half a second
+
+
+def switch_thread():
+    direction = 'right'
+    while True:
+        time.sleep(60)  # wait for 60 seconds
+        # switch to the next desktop
+        pyautogui.hotkey('ctrl', 'alt', direction)
+        if direction == 'left':
+            direction = 'right'
+        else:
+            direction = 'left'
+
+
+if __name__ == '__main__':
+    threading.Thread(target=click_thread).start()
+    #threading.Thread(target=scroll_thread).start() #uncomment this line if you want to scroll.
+    # threading.Thread(target=switch_thread).start() #uncomment this line if you want switch the desktops.
